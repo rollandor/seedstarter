459ff8172f67bde8e965dc654c79351c89442ef0
Rolland <rolanddoor@gmail.com>
2023-11-08 11:50:05 +0500

    [blockchain] deploy token to sepolia

--- contracts/SeedstarterToken.sol
+++ contracts/SeedstarterToken.sol
@@ -1,10 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract SeedstarterToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("SeedstarterToken", "SDS") {
-        _mint(msg.sender, initialSupply);
+contract Seedstarter is ERC20, ERC20Burnable, Ownable, ERC20Permit {
+    constructor(address initialOwner)
+        ERC20("Seedstarter", "SDS")
+        Ownable(initialOwner)
+        ERC20Permit("Seedstarter")
+    {
+        _mint(msg.sender, 20000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
     }
 }
