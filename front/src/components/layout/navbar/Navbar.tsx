94d5d4db3192e90e0475e04165a018fce6d14c48
Rolland <rolanddoor@gmail.com>
2024-01-15 15:54:39 +0500

    [blockchain] implemented the first prototype of interaction with blockchain - added connecting to metamask

--- front/src/components/layout/navbar/Navbar.tsx
+++ front/src/components/layout/navbar/Navbar.tsx
@@ -1,39 +1,113 @@
+"use client"
+
 import Image from "next/image";
 import React from "react";
 import styles from '@/components/layout/navbar/Navbar.module.scss'
 import { MENU } from "./Navbar.data";
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
-const Button = (text: string) => {
-  return (
-    <button className='h-[36px] bg-secondary border-secondary border rounded-md inline-flex items-center justify-center py-3 px-4 text-center text-base font-bold bg-[#0BB489] text-white hover:bg-[#0BB489] hover:border-[#0BB489] disabled:bg-gray-3 disabled:border-gray-3 disabled:text-dark-5'>
-      {text}
-    </button>
-  )
-}
+import Metamask from "@/components/metamask/metamask";
+import { signMessage } from "@/components/metamask/sign"
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-      <div className='w-4/5 flex flex-col justify-between'>
-        <img className='w-[245px] pt-8 pl-7' src='/menu_logo_green.svg'/>
-        <ul className='h-10 text-[#8060C8] flex justify-around items-center'>
-          {MENU.map(item => (
-            <li key={item.url} className="px-4">
-              <Link href={item.url}>{item.name}</Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+  const [haveMetamask, setHaveMetamask] = useState(true);
+  const [client, setClient] = useState({
+    isConnected: false,
+    address: "",
+  });
 
-      <div className='px-4 flex flex-col justify-center'>
-        <div className='py-2 flex items-center'>
-          <span className='text-[#0BB489]'>Welcome, User!</span>
-          <img className='px-4' src='/male_user.svg'/>
+  const checkConnection = async () => {
+    const { ethereum }: any = window;
+    if (ethereum) {
+      setHaveMetamask(true);
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length > 0) {
+        setClient({
+          isConnected: true,
+          address: accounts[0],
+        });
+      } else {
+        setClient({
+          isConnected: false,
+          address: "",
+        });
+      }
+    } else {
+      setHaveMetamask(false);
+    }
+  };
+
+  const connectWeb3 = async () => {
+    try {
+      const { ethereum }: any = window;
+
+      if (!ethereum) {
+        console.log("Metamask not detected");
+        return;
+      }
+
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+
+      setClient({
+        isConnected: true,
+        address: accounts[0],
+      });
+    } catch (error) {
+      console.log("Error connecting to metamask", error);
+    }
+  };
+
+  useEffect(() => {
+    checkConnection();
+  }, []);
+
+  return (
+    <>
+    {/* Navbar */}
+      <nav className={styles.navbar}>
+        <div className='w-4/5 flex flex-col justify-between'>
+          <img className='w-[245px] pt-8 pl-7' src='/menu_logo_green.svg' />
+          <ul className='h-10 text-[#8060C8] flex justify-around items-center'>
+            {MENU.map(item => (
+              <li key={item.url} className="px-4">
+                <Link href={item.url}>{item.name}</Link>
+              </li>
+            ))}
+          </ul>
         </div>
-        {Button('Connect wallet')}
-      </div>
-    </nav>
+
+        <div className='px-4 flex flex-col justify-center'>
+          <div className='py-2 flex items-center'>
+            <span className='text-[#0BB489]'>Welcome, User!</span>
+            <img className='px-4' src='/male_user.svg' />
+          </div>
+
+          {/* ---- */}
+          <p>
+            {!haveMetamask ? (
+              <Metamask />
+            ) : client.isConnected ? (
+              <button className={styles['connect_wallet']} >
+                {client.address.slice(0, 4)}...
+                {client.address.slice(38, 42)}
+              </button>
+            ) : (
+              <>
+                <br />
+                <button className={styles['connect_wallet']} onClick={connectWeb3}>
+                  Connect Wallet
+                </button>
+              </>
+            )}
+          </p>
+
+        </div>
+      </nav>
+      {/* Navbar end */}
+    </>
   )
 };
 
