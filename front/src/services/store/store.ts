7e5297e67de9b458c3d010023cb0bceb0f041630
Rolland <rolanddoor@gmail.com>
2024-01-25 14:14:06 +0500

    [back] added store by zustand

---
+++ front/src/services/store/store.ts
@@ -0,0 +1,27 @@
+import create from "zustand";
+import * as chains from "viem/chains";
+import { ChainWithAttributes } from "@/utils";
+
+
+/**
+ * Zustand Store
+ *
+ * You can add global state to the app using this useGlobalState, to get & set
+ * values from anywhere in the app.
+ *
+ * Think about it as a global useState.
+ */
+
+type GlobalState = {
+  nativeCurrencyPrice: number;
+  setNativeCurrencyPrice: (newNativeCurrencyPriceState: number) => void;
+  targetNetwork: ChainWithAttributes;
+  setTargetNetwork: (newTargetNetwork: ChainWithAttributes) => void;
+};
+
+export const useGlobalState = create<GlobalState>(set => ({
+  nativeCurrencyPrice: 0,
+  setNativeCurrencyPrice: (newValue: number): void => set(() => ({ nativeCurrencyPrice: newValue })),
+  targetNetwork: chains.sepolia,
+  setTargetNetwork: (newTargetNetwork: ChainWithAttributes) => set(() => ({ targetNetwork: newTargetNetwork })),
+}));
