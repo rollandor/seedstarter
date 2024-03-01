fe9912c6d255920f9abcf50271434dc36e040d06
Vladislav <rolanddoor@gmail.com>
2024-03-01 18:53:58 +0500

    [back] added independent components

--- front/src/app/api/auth/logout/route.ts
+++ front/src/app/api/auth/logout/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
 
 export async function GET(req: Request) {
+  const cks = cookies();
+  console.log(cks.getAll())
+
   let res = NextResponse.json(
     { message: 'Logout success' },
     { status: 200 },
