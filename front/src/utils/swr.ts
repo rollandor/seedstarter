9e73fee0e001a958a18034f595b8094658e6f775
Vladislav <rolanddoor@gmail.com>
2024-03-01 18:47:50 +0500

    [back] added SWR

---
+++ front/src/utils/swr.ts
@@ -0,0 +1,37 @@
+import type { User } from '@prisma/client';
+import useSWRImmutable from 'swr/immutable';
+
+async function fetcher<T>(
+  input: RequestInfo | URL,
+  init?: RequestInit | undefined
+): Promise<T> {
+  return fetch(input, init).then((res) => res.json());
+}
+
+export function useUser() {
+  const { data, error, mutate } = useSWRImmutable<any>(
+    '/api/auth/user',
+    (url) => fetcher(url, { credentials: 'include' }),
+    {
+      onErrorRetry(err, key, config, revalidate, revalidateOpts) {
+        return false;
+      }
+    }
+  );
+
+  if (error || data?.message) {
+    console.log(error || data?.message);
+
+    return {
+      user: undefined,
+      accessToken: undefined,
+      mutate
+    };
+  }
+
+  return {
+    user: data?.user as User,
+    accessToken: data?.accessToken as string,
+    mutate
+  };
+}
