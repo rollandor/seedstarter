3e56c3e2962ec2903ac495c38f48143d90038e43
Vladislav <rolanddoor@gmail.com>
2024-03-01 18:39:47 +0500

    [other] fix pino-pretty

--- front/next.config.js
+++ front/next.config.js
@@ -4,7 +4,11 @@ const nextConfig = {
     SDS_ADDR: process.env.SDS_ADDR,
     SDS_PRESALE_ADDR: process.env.SDS_PRESALE_ADDR,
     WALLET_CLOUD_PROJECT_ID: process.env.WALLET_CLOUD_PROJECT_ID,
-  }
+  },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
 }
 
 module.exports = nextConfig
